3c73fd9991e9dfd7c7cdaffd8391c7b9979a70bf
RL24 <richardlapwood24@gmail.com>
2017-04-17 09:00:01 +1200

    Create empty C++ CLion project
    Signed-off-by: RL24 <richardlapwood24@gmail.com>

---
+++ I309/cmake-build-debug/CMakeFiles/I309.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/I309.dir/main.cpp.obj"
+  "I309.pdb"
+  "I309.exe"
+  "I309.exe.manifest"
+  "libI309.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/I309.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
