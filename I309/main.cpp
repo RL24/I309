84461f2023643fcca472b2391fd416d7cfe3990a
RL24 <richardlapwood24@gmail.com>
2017-05-06 21:13:17 +1200

    Add minor comments for explanation Add welcome message Add user input
    Signed-off-by: RL24 <richardlapwood24@gmail.com>

--- I309/main.cpp
+++ I309/main.cpp
@@ -1,7 +1,23 @@
+//Include necessary libraries
 #include <iostream>
 
+//Disable the need to use "std::<method>"
 using namespace std;
 
+//Main function
 int main() {
+    //Initial welcome message
+    cout << "Welcome to a console sudoku solver. Please enter each of the 9 rows:" << endl;
+    cout << "(tip: use ? or - for unknown/blank squares in the sudoku)" << endl;
+    cout << "(spaces are optional, e.g.: ??? 4?7 ?9?)" << endl << endl;
+
+    //Array to store the users input
+    string rows[9];
+
+    //Get the users input
+    for (int i = 0; i < 9; i++) {
+        getline(cin, rows[i]);
+    }
+
     return 0;
 }
