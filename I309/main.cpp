d3b714e5c3fcb3080ef7a6b26d3a8fd506509303
RL24 <richardlapwood24@gmail.com>
2017-06-07 08:45:36 +1200

    Add isEmpty method Add solve method
    Fix the solve method, use recursion
    
    Remove unused lib includes
    Remove unused methods
    
    Signed-off-by: RL24 <richardlapwood24@gmail.com>

--- I309/main.cpp
+++ I309/main.cpp
@@ -1,37 +1,27 @@
 //Include necessary libraries
 #include <iostream>
-#include <cmath>
-#include <vector>
-#include <list>
-#include <sstream>
 
 //Disable the need to use "std::<method>"
 using namespace std;
 
-//Create some constant non-changing values
-const int NONET_SIZE = 3;
-const int GRID_SIZE = NONET_SIZE * 3;
+//Create some constant non-changing values (grid size, and empty value)
+const int GS = 9;
+const int UV = -1;
 
 //Declare methods (must be done prior to method calls)
 bool isRowValid(string *row);
 
 void replaceString(string *str, string from, string to);
 
-bool inRow(int grid[GRID_SIZE][GRID_SIZE], int row, int value);
+bool isEmpty(int grid[GS][GS], int &row, int &col);
 
-bool inRowPos(int grid[GRID_SIZE][GRID_SIZE][9], int row, int value);
+bool inRow(int grid[GS][GS], int row, int value);
 
-bool inCol(int grid[GRID_SIZE][GRID_SIZE], int col, int value);
+bool inCol(int grid[GS][GS], int col, int value);
 
-bool inColPos(int grid[GRID_SIZE][GRID_SIZE][9], int col, int value);
+bool inBox(int grid[GS][GS], int row, int col, int value);
 
-bool inBox(int grid[GRID_SIZE][GRID_SIZE], int row, int col, int value);
-
-bool inBoxPos(int grid[GRID_SIZE][GRID_SIZE][9], int row, int col, int value);
-
-float floor(float value, float incremental);
-
-int floor(int value, int incremental);
+bool solve(int grid[GS][GS]);
 
 //Main function
 int main() {
@@ -41,10 +31,10 @@ int main() {
     cout << "(spaces are optional, e.g.: ??? 4?7 ?9?)" << endl << endl;
 
     //Array to store the users input
-    string rows[GRID_SIZE];
+    string rows[GS];
 
     //Get the users input
-    for (int i = 0; i < GRID_SIZE; i++) {
+    for (int i = 0; i < GS; i++) {
         getline(cin, rows[i]);
 
         //Ensure the user actually entered something (or that they entered a blank line to fulfill the layout of a sudoku)
@@ -58,7 +48,7 @@ int main() {
     }
 
     //Validate each row and ensure the user inputs a new row for every invalid one
-    for (int i = 0; i < GRID_SIZE; i++) {
+    for (int i = 0; i < GS; i++) {
         while (!isRowValid(&rows[i])) {
             system("cls");
             cout << "Row " << i + 1 << " is invalid:" << endl;
@@ -73,80 +63,25 @@ int main() {
 
     cout << "Validated" << endl;
 
-    int grid[GRID_SIZE][GRID_SIZE];
-    int predefined[GRID_SIZE][GRID_SIZE];
+    int grid[GS][GS];
 
     //Convert the user input to a more usable format
-    for (int x = 0; x < GRID_SIZE; x++) {
-        for (int y = 0; y < GRID_SIZE; y++) {
+    for (int x = 0; x < GS; x++) {
+        for (int y = 0; y < GS; y++) {
             //Negate by the int value of '0' to ensure the content of value is 0-9
             int value = rows[x][y] - '0';
 
-            //If the value is not 0-9, set to -1 to represent an unknown grid cell
+            //If the value is not 0-9, set to "UV" to represent an unknown grid cell
             if (value < 0 || value > 9)
-                value = -1;
+                value = UV;
 
-            predefined[x][y] = value;
             grid[x][y] = value;
         }
     }
+
     cout << "Created grid" << endl;
 
-    int possibilities[GRID_SIZE][GRID_SIZE][9];
-    for (int x = 0; x < GRID_SIZE; x++)
-        for (int y = 0; y < GRID_SIZE; y++)
-            for (int z = 0; z < 9; z++)
-                possibilities[x][y][z] = -1;
-
-    bool solved = false;
-    while (!solved) {
-        solved = true;
-        for (int x = 0; x < GRID_SIZE; x++) {
-            for (int y = 0; y < GRID_SIZE; y++) {
-                if (grid[x][y] != -1)
-                    continue;
-
-                auto boxX = (int) (ceil(x / 3) * 3);
-                auto boxY = (int) (ceil(y / 3) * 3);
-
-                for (int z = 1; z <= 9; z++) {
-                    if (!inRow(grid, x, z) && !inCol(grid, y, z)/* && !inBox(grid, boxX, boxY, z)*/) {
-                        bool set = false;
-                        for (int w = 0; w < 9; w++) {
-                            if (possibilities[x][y][w] == -1) {
-                                possibilities[x][y][w] = z;
-                                set = true;
-                                break;
-                            }
-                        }
-                        if (set)
-                            break;
-                    }
-                }
-            }
-        }
-
-        for (int x = 0; x < GRID_SIZE; x++) {
-            for (int y = 0; y < GRID_SIZE; y++) {
-                if (grid[x][y] != -1)
-                    continue;
-
-                solved = false;
-
-                auto boxX = (int) (ceil(x / 3) * 3);
-                auto boxY = (int) (ceil(y / 3) * 3);
-
-                //Iterate over the possibles and set them to -1 if its in another cell in the row col or boxes possibilities
-                for (int z = 1; z <= 9; z++) {
-                    if (!inRowPos(possibilities, x, z) && !inColPos(possibilities, y, z)/* && !inBoxPos(possibilities, boxX, boxY, z)*/) {
-                        grid[x][y] = z;
-                        break;
-                    }
-                }
-            }
-        }
-    }
-    //solveSudoku(grid, 0, 0);
+    solve(grid);
 
     //TODO: solve the sudoku, and clear the console window and print out the percentage of sudoku completion
 
@@ -154,10 +89,10 @@ int main() {
     //system("cls");
 
     //Print out the final sudoku solution
-    for (int x = 0; x < GRID_SIZE; x++) {
-        for (int y = 0; y < GRID_SIZE; y++) {
+    for (int x = 0; x < GS; x++) {
+        for (int y = 0; y < GS; y++) {
             int value = grid[x][y];
-            cout << (value == -1 ? "-" : to_string(value));
+            cout << (value == UV ? "-" : to_string(value));
             if ((y + 1) % 3 == 0)
                 cout << " ";
         }
@@ -181,7 +116,7 @@ int main() {
  */
 bool isRowValid(string *row) {
     int len = row->length();
-    return len == GRID_SIZE && row->find_first_not_of("0123456789?-") == string::npos;
+    return len == GS && row->find_first_not_of("0123456789?-") == string::npos;
 }
 
 /**
@@ -199,6 +134,14 @@ void replaceString(string *str, string from, string to) {
     }
 }
 
+bool isEmpty(int grid[GS][GS], int &row, int &col) {
+    for (row = 0; row < GS; row++)
+        for (col = 0; col < GS; col++)
+            if (grid[row][col] == UV)
+                return true;
+    return false;
+}
+
 /**
  * Check if the given value is in the specified row in the sudoku
  * @param grid The sudoku grid of known values
@@ -206,25 +149,10 @@ void replaceString(string *str, string from, string to) {
  * @param value The value to check
  * @return If the row contains the value
  */
-bool inRow(int grid[GRID_SIZE][GRID_SIZE], int row, int value) {
-    for (int x = 0; x < GRID_SIZE; x++)
+bool inRow(int grid[GS][GS], int row, int value) {
+    for (int x = 0; x < GS; x++)
         if (grid[row][x] == value)
             return true;
-    return false;
-}
-
-/**
- * Check if the given value is in the specified rows possibilities in the sudoku
- * @param grid The sudoku grid of known values
- * @param row The row to check in
- * @param value The value to check
- * @return If the rows possibilities contains the value
- */
-bool inRowPos(int grid[GRID_SIZE][GRID_SIZE][9], int row, int value) {
-    for (int x = 0; x < GRID_SIZE; x++)
-        for (int z = 0; z < 9; z++)
-            if (value != -1 && grid[row][x][z] == value)
-                return true;
     return false;
 }
 
@@ -235,25 +163,10 @@ bool inRowPos(int grid[GRID_SIZE][GRID_SIZE][9], int row, int value) {
  * @param value The value to check
  * @return If the column contains the value
  */
-bool inCol(int grid[GRID_SIZE][GRID_SIZE], int col, int value) {
-    for (int y = 0; y < GRID_SIZE; y++)
+bool inCol(int grid[GS][GS], int col, int value) {
+    for (int y = 0; y < GS; y++)
         if (grid[y][col] == value)
             return true;
-    return false;
-}
-
-/**
- * Check if the given value is in the specified columns possibilities in the sudoku
- * @param grid The sudoku grid of known values
- * @param col The column to check in
- * @param value The value to check
- * @return If the columns possibilities contains the value
- */
-bool inColPos(int grid[GRID_SIZE][GRID_SIZE][9], int col, int value) {
-    for (int y = 0; y < GRID_SIZE; y++)
-        for (int z = 0; z < 9; z++)
-            if (value != -1 && grid[y][col][z] == value)
-                return true;
     return false;
 }
 
@@ -265,7 +178,7 @@ bool inColPos(int grid[GRID_SIZE][GRID_SIZE][9], int col, int value) {
  * @param value The value to check
  * @return If the box contains the value
  */
-bool inBox(int grid[GRID_SIZE][GRID_SIZE], int row, int col, int value) {
+bool inBox(int grid[GS][GS], int row, int col, int value) {
     for (int x = row; x < row + NONET_SIZE; x++)
         for (int y = col; y < col + NONET_SIZE; y++)
             if (grid[x][y] == value)
@@ -274,34 +187,27 @@ bool inBox(int grid[GRID_SIZE][GRID_SIZE], int row, int col, int value) {
 }
 
 /**
- * Check if the given value is in the specified boxes possibilities in the sudoku
- * @param grid The sudoku grid possibilities of known values
- * @param row The base row of the box
- * @param col The base column of the box
- * @param value The value to check
- * @return If the boxes possibilities contains the value
+ * Attempt to solve the sudoku using recursion
+ * @param grid The sudoku grid of known values
+ * @param row The row to check
+ * @param col The column to check
+ * @return If the cell has been solved
  */
-bool inBoxPos(int grid[GRID_SIZE][GRID_SIZE][9], int row, int col, int value) {
-    for (int x = row; x < row + NONET_SIZE; x++)
-        for (int y = col; y < col + NONET_SIZE; y++)
-            for (int z = 0; z < 9; z++)
-                if (value != -1 && grid[x][y][z] == value)
-                    return true;
+bool solve(int grid[GS][GS]) {
+    int row, col;
+    //Get the next empty cell, set "row" and "cell" values of the pointer to the empty cell
+    if (!isEmpty(grid, row, col))
+        return true;
+    for (int i = 1; i <= 9; i++) {
+        //Check if the value "i" is not found in any other cell in the same row, column, and box
+        if (!inRow(grid, row, i) && !inCol(grid, col, i) && !inBox(grid, row - row % 3, col - col % 3, i)) {
+            grid[row][col] = i;
+            //Use recursion to solve other cells, if they all return true in the end, then the sudoku has been solved
+            if (solve(grid))
+                return true;
+            //Otherwise set the cell to the designated "empty" value
+            grid[row][col] = UV;
+        }
+    }
     return false;
-}
-
-/**
- * Floor a value to a given incremental multiplier (e.g. floor 75 to the nearest 20 mutliplier = 80 (20 * 4 = 80, 20 * 3 = 60, 75 is closer to 80))
- * @param value The value to floor
- * @param incremental The incremental value to floor to a multiplier of
- * @return The floored value to the nearest incremental multiplier
- */
-float floor(float value, float incremental) {
-    float one = 1 / incremental;
-    return floor(value * one) / one;
-}
-
-/**Integer overload for float floor()**/
-int floor(int value, int incremental) {
-    return (int) floor((float) value, (float) incremental);
 }
